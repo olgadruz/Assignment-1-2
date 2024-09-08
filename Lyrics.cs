f762e405cc01528a7d3c8742dfea50a16f65e3fe
Оля Дружкова <Druzhkova@MacBook-Pro-Ola.local>
2024-09-08 13:05:52 -0400

    first draft

---
+++ Lyrics.cs
@@ -0,0 +1,12 @@
+﻿namespace Lyrics;
+
+class Lyrics
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Coming out of my cage");
+        Console.WriteLine("And I've been doing just fine");
+        Console.WriteLine("Gotta gotta be down");
+        Console.WriteLine("Because I want it all");
+    }
+}
